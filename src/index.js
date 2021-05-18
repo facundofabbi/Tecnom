e4cc4d3e38626f18e9a13a4b9bf3555851ee193d
facundofabbi <62101931+facundofabbi@users.noreply.github.com>
2021-05-18 01:25:22 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -22,10 +22,11 @@ Amplify.default.configure({
 app.set('port', 4000)
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/index.html'));
+    res.send("hola")
+    //res.sendFile(path.join(__dirname,'views/index.html'));
 })
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'),()=>{
     console.log("tecnom")
